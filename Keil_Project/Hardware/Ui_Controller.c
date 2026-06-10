a5f3205f45efd97185e42b9c5a94f0d27fe9b32c
Ran-sh <483763727@qq.com>
2026-06-10 19:53:01 +0800

    fix: 清理最后一处 S_BACK_DBL 残留

--- Keil_Project/Hardware/Ui_Controller.c
+++ Keil_Project/Hardware/Ui_Controller.c
@@ -352,7 +352,7 @@ static void Draw_Run_Main(void)
             Tft_Driver_Show_CN_String(4, 1, buf, UI_COLOR_TEXT, UI_COLOR_BG);
         }
         Tft_Driver_Show_CN_String(6, Right("OFF:" S_STOP), "OFF:" S_STOP, UI_COLOR_TEXT, UI_COLOR_BG);
-        Tft_Driver_Show_CN_String(7, 0,  S_BACK_DBL,     UI_COLOR_TEXT, UI_COLOR_BG);
+        Tft_Driver_Show_CN_String(7, 0,  S_PAGE_NO_WIFI,     UI_COLOR_TEXT, UI_COLOR_BG);
         Tft_Driver_Show_CN_String(7, Right(S_PAGE),      "\xe5\x88\x87\xe9\xa1\xb5V",    UI_COLOR_TEXT, UI_COLOR_BG);
         break;
     }
